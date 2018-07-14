e849f645caef00a007b87ff1f5e37408feddbdd7
dector <dector9@gmail.com>
2018-07-14 19:39:35 +0200

    Read content from files

--- src/main/kotlin/io/github/dector/glow/v2/dumbimpl/data_providers.kt
+++ src/main/kotlin/io/github/dector/glow/v2/dumbimpl/data_providers.kt
@@ -3,44 +3,21 @@ package io.github.dector.glow.v2.dumbimpl
 import io.github.dector.glow.v2.BlogData
 import io.github.dector.glow.v2.DataProvider
 import io.github.dector.glow.v2.Page
+import java.io.File
+import java.io.FilenameFilter
 
 
 val dumbDataProvider: DataProvider = {
-    val content = listOf(
-            """
-            |---
-            |title: Page #1
-            |tags: blog, published, first
-            |---
-            |
-            |This is **FIRST** page.
-            """.trimMargin(),
-            """
-            |---
-            |title: Page #2
-            |tags: blog, published, second
-            |---
-            |
-            |This is **SECOND** page.
-            """.trimMargin(),
-            """
-            |---
-            |title: Page #3
-            |tags: blog, published, third
-            |---
-            |
-            |This is **THIRD** page.
-            """.trimMargin(),
-            """
-            |---
-            |title: Page #4
-            |tags: blog, draft
-            |draft = true
-            |---
-            |
-            |This is **DRAFT** page. Should not be displayed.
-            """.trimMargin()
-    )
+    val content = loadFiles("v2/source/")
 
     dumbMdToHtmlConverter(content)
+}
+
+private fun loadFiles(projectDir: String): List<String> {
+    val dir = File(projectDir, "posts")
+
+    // TODO check errors
+
+    return dir.listFiles(FilenameFilter { dir, name -> name.endsWith(".md") })
+            .map { it.readText() }
 }
