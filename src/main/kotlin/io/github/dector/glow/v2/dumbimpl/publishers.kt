ed480183d785f3f1cf44e56cefab08b0f0af61f0
dector <dector9@gmail.com>
2018-07-15 18:49:47 +0200

    Cleanup old code

--- src/main/kotlin/io/github/dector/glow/v2/dumbimpl/publishers.kt
+++ src/main/kotlin/io/github/dector/glow/v2/dumbimpl/publishers.kt
@@ -24,54 +24,5 @@ val dumbDataPublisher: DataPublisher = { data ->
     data.indexPages.forEach (::writeToFile)
     data.tagPages.forEach (::writeToFile)
 
-    /*
-
-    // Index posts
-    block("INDEX PAGES total: ${data.indexPages.size}") {
-        data.indexPages.forEach {
-            page("--- --- PAGE ${it.pageNumber}/${it.totalPages} --- ---") {
-                println(it.articles.joinToString(separator = "\n\n") {
-                    """ |${it.title}
-                        |${it.content}
-                        |${it.tags.joinToString(prefix = "[", postfix = "]")}
-                    """.trimMargin()
-                })
-                println()
-                println("<< Prev: ${it.prevPage?.pageNumber}")
-                println("Next: ${it.nextPage?.pageNumber} >>")
-            }
-        }
-    }
-
-    // Pages
-    block("PAGES total: ${data.posts.size}") {
-        data.posts.forEach {
-            page("--- --- --- --- --- ---") {
-                println("""
-                        |${it.title}
-                        |${it.content}
-                        |${it.tags.joinToString(prefix = "[", postfix = "]")}
-                        |<< Prev: ${it.prevPage?.title}
-                        |Next: ${it.nextPage?.title} >>
-                    """.trimMargin()
-                )
-            }
-        }
-    }
-
-    // Tag posts
-    block("TAG PAGES total: ${data.tagPages.size}") {
-        data.tagPages.forEach {
-            page("--- --- TAG: ${it.tag} --- ---") {
-                println(it.articles.joinToString(separator = "\n\n") {
-                    """ |${it.title}
-                        |${it.content}
-                        |${it.tags.joinToString(prefix = "[", postfix = "]")}
-                    """.trimMargin()
-                })
-            }
-        }
-    }
-*/
     PublishResult.Success()
 }
