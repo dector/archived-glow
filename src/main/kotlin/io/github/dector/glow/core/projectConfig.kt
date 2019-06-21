88c4aaf05797bd81831cd465a80a20c493d802b0
Denys M <dector9@gmail.com>
2019-06-21 21:34:21 +0300

    Watch on file system for `glow serve`.

--- src/main/kotlin/io/github/dector/glow/core/projectConfig.kt
+++ src/main/kotlin/io/github/dector/glow/core/projectConfig.kt
@@ -8,67 +8,69 @@ import java.io.File
 fun mockProjectsConfig() = ProdConfig
 
 fun provideBlogVM() = BlogVM(   // Convert to VM later, provide setup model
-        title = "Dead Art Space",
-        navigation = listOf(
-                // FIXME
+    title = "Dead Art Space",
+    navigation = listOf(
+        // FIXME
 //                NavigationItem("/", "Home", Home),
 //                NavigationItem("/notes", "Notes", Notes, visible = false),
-                NavigationItem("/", "Notes", Notes)//,
+        NavigationItem("/", "Notes", Notes)//,
 //                NavigationItem("https://forms.gle/PDqcYSiBY8Y4iVP5A", "Feedback", Feedback)
 //                NavigationItem("/projects", "Projects", Projects),
 //                NavigationItem("/about", "About", About)
-        ),
-        footer = FooterVM(
-                author = "Dead Art Space",
-                year = "2019",
-                licenseName = "CC BY-SA 4.0",
-                licenseUrl = "http://creativecommons.org/licenses/by-sa/4.0/"
-        )
+    ),
+    footer = FooterVM(
+        author = "Dead Art Space",
+        year = "2019",
+        licenseName = "CC BY-SA 4.0",
+        licenseUrl = "http://creativecommons.org/licenses/by-sa/4.0/"
+    )
 )
 
 private val TestConfig = ProjectConfig(
-        input = InputConfig(
-                staticFolder = File("v2/themes/plain/source" + (if (DevMode) "-dev" else "")),
-                pagesFolder = File("v2/src/pages"),
-                notesFolder = File("v2/src/notes")
-        ),
-        output = OutputConfig(
-                outputFolder = File("v2/out2/"),
+    input = InputConfig(
+        sourcesFolder = File("v2/src/"),
+        staticFolder = File("v2/themes/plain/source" + (if (DevMode) "-dev" else "")),
+        pagesFolder = File("v2/src/pages"),
+        notesFolder = File("v2/src/notes")
+    ),
+    output = OutputConfig(
+        outputFolder = File("v2/out2/"),
 
-                staticFolder = File("v2/out2/"),
-                notesPath = "/notes",
-                overrideFiles = true
-        ),
-        navigation = provideBlogVM().navigation
+        staticFolder = File("v2/out2/"),
+        notesPath = "/notes",
+        overrideFiles = true
+    ),
+    navigation = provideBlogVM().navigation
 )
 
 private val ProdConfig = ProjectConfig(
-        input = InputConfig(
-                staticFolder = File("v2/themes/dead-art/source" + (if (DevMode) "-dev" else "")),
-                pagesFolder = File("website/src/pages"),
-                notesFolder = File("website/src/notes")
-        ),
-        output = OutputConfig(
-                outputFolder = File("v2/website-out/"),
+    input = InputConfig(
+        sourcesFolder = File("website/src/"),
+        staticFolder = File("v2/themes/dead-art/source" + (if (DevMode) "-dev" else "")),
+        pagesFolder = File("website/src/pages"),
+        notesFolder = File("website/src/notes")
+    ),
+    output = OutputConfig(
+        outputFolder = File("v2/website-out/"),
 
-                staticFolder = File("v2/website-out/"),
-                notesPath = "/notes",
-                overrideFiles = true
-        ),
-        navigation = provideBlogVM().navigation
+        staticFolder = File("v2/website-out/"),
+        notesPath = "/notes",
+        overrideFiles = true
+    ),
+    navigation = provideBlogVM().navigation
 )
 
 data class ProjectConfig(
-        val input: InputConfig,
-        val output: OutputConfig,
-        val navigation: List<NavigationItem>
+    val input: InputConfig,
+    val output: OutputConfig,
+    val navigation: List<NavigationItem>
 )
 
 data class NavigationItem(
-        val path: String,
-        val title: String,
-        val type: NavItemType,
-        val visible: Boolean = true
+    val path: String,
+    val title: String,
+    val type: NavItemType,
+    val visible: Boolean = true
 )
 
 enum class NavItemType {
@@ -77,17 +79,17 @@ enum class NavItemType {
 }
 
 data class InputConfig(
-        val staticFolder: File,
-        val pagesFolder: File,
-        val notesFolder: File
+    val sourcesFolder: File,
+    val staticFolder: File,
+    val pagesFolder: File,
+    val notesFolder: File
 )
 
-
 data class OutputConfig(
-        val outputFolder: File,
-        val notesPath: String,
+    val outputFolder: File,
+    val notesPath: String,
 
-        val staticFolder: File,
-        val overrideFiles: Boolean = false
+    val staticFolder: File,
+    val overrideFiles: Boolean = false
 )
 
--- src/main/kotlin/io/github/dector/glow/server/Server.kt
+++ src/main/kotlin/io/github/dector/glow/server/Server.kt
@@ -1,42 +1,39 @@
 package io.github.dector.glow.server
 
-import io.github.dector.glow.core.HtmlWebPageContent
-import io.github.dector.glow.core.WebPage
-import io.github.dector.glow.core.WebPagePath
+import io.github.dector.glow.core.*
 import io.github.dector.glow.core.components.DataPublisher
 import io.github.dector.glow.core.components.GlowEngine
 import io.github.dector.glow.core.components.InMemoryDataPublisher
-import io.github.dector.glow.core.isIndex
 import io.github.dector.glow.di.DI
+import io.github.dector.glow.di.get
 import io.github.dector.glow.parentFolder
 import io.javalin.Javalin
 import org.koin.dsl.module
+import java.nio.file.FileSystems
+import java.nio.file.FileVisitResult
+import java.nio.file.Files.walkFileTree
+import java.nio.file.Path
+import java.nio.file.SimpleFileVisitor
+import java.nio.file.StandardWatchEventKinds.*
+import java.nio.file.attribute.BasicFileAttributes
 
 class Server {
 
     private val pagesStorage = mutableSetOf<WebPage>()
+    private lateinit var app: Javalin
 
     init {
         prepareServerDependencies()
     }
 
     fun run() {
-        print("Running server... ")
+        buildAndServeBlog()
 
-        val app = runServer()
+        val projectConfig = DI.get<ProjectConfig>()
+        println("Serving ${projectConfig.input.sourcesFolder.absolutePath}")
 
-        println("on port ${app.port()}")
-
-        println("Building blog...")
-        DI.get(GlowEngine::class)
-            .execute()
-
-        pagesStorage.forEach { page ->
-            app.serve(page.path, page.content)
-
-            if (page.path.isIndex) {
-                app.serve(page.path.parentFolder(), page.content)
-            }
+        watchForBlogSources(projectConfig) {
+            buildAndServeBlog()
         }
     }
 
@@ -49,6 +46,68 @@ class Server {
             })
         }
     }
+
+    private fun watchForBlogSources(config: ProjectConfig, body: () -> Unit) {
+        val watcher = FileSystems.getDefault().newWatchService()
+        val path = config.input.sourcesFolder.toPath()
+
+        walkFileTree(path, object : SimpleFileVisitor<Path>() {
+            override fun preVisitDirectory(dir: Path, attrs: BasicFileAttributes?): FileVisitResult {
+                dir.register(watcher,
+                    ENTRY_CREATE,
+                    ENTRY_MODIFY,
+                    ENTRY_DELETE)
+                return FileVisitResult.CONTINUE
+            }
+        })
+
+        var process = true
+        while (process) {
+            val watcherKey = watcher.take()
+            if (watcherKey == null) {
+                process = false
+                continue
+            }
+
+            val events = watcherKey.pollEvents()
+            if (events.isNotEmpty())
+                body()
+
+            watcherKey.reset()
+        }
+    }
+
+    private fun buildAndServeBlog() {
+        println("Building blog...")
+        pagesStorage.clear()
+        DI.get(GlowEngine::class)
+            .execute()
+
+        restartServer()
+        pagesStorage.forEach { page ->
+            app.serve(page.path, page.content)
+
+            if (page.path.isIndex) {
+                app.serve(page.path.parentFolder(), page.content)
+            }
+        }
+
+        println("Ready!")
+    }
+
+    private fun restartServer() {
+        if (::app.isInitialized) {
+            println("Stopping server...")
+
+            app.stop()
+        }
+
+        print("Running server... ")
+
+        app = Javalin.create().start(9217)
+
+        println("on port ${app.port()}")
+    }
 }
 
 private fun Javalin.serve(path: WebPagePath, content: HtmlWebPageContent) {
@@ -56,7 +115,3 @@ private fun Javalin.serve(path: WebPagePath, content: HtmlWebPageContent) {
         ctx.html(content.value)
     }
 }
-
-private fun runServer() = Javalin
-    .create()
-    .start(9217)
