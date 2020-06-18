fb47ab280dc525765329cd8bbd00de95991ded01
Denys M <dector9@gmail.com>
2020-06-18 18:12:10 +0400

    Include projects from `components/` dir

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,3 +6,15 @@ include(
     "glow-cli",
     "templates-hyde"
 )
+
+// Include projects from `components/` dir
+File("components")
+    .listFiles()!!
+    .asSequence()
+    .filter { it.isDirectory }
+    .filter { File(it, "build.gradle.kts").exists() }
+    .forEach { dir ->
+        val projectName = ":${dir.name}"
+        include(projectName)
+        project(projectName).projectDir = File("${dir.parent}/${dir.name}")
+    }
