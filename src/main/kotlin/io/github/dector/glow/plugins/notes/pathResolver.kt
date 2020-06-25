33a863b010de0f5936a1fe4bd4efa1845f113d49
Denys M <dector9@gmail.com>
2020-06-25 14:14:04 +0400

    Fix draft note link

--- src/main/kotlin/io/github/dector/glow/plugins/notes/pathResolver.kt
+++ src/main/kotlin/io/github/dector/glow/plugins/notes/pathResolver.kt
@@ -1,7 +1,6 @@
 package io.github.dector.glow.plugins.notes
 
 import io.github.dector.glow.config.LegacyProjectConfig
-import io.github.dector.glow.core.Empty
 import io.github.dector.glow.core.WebPagePath
 import io.github.dector.glow.core.path.cleanupTitleForWebPath
 import java.time.ZoneOffset
@@ -29,9 +28,9 @@ class NotesWebPathResolver(
         .withZone(ZoneOffset.UTC)
 
     override fun resolve(note: Note, buildUrlPath: Boolean): WebPagePath {
-        note.publishedAt ?: return WebPagePath.Empty
-
-        val dir = notePathDateFormatter.format(note.publishedAt)
+        val dir = if (note.publishedAt != null)
+            notePathDateFormatter.format(note.publishedAt)
+        else DraftsDirName
         val instancePath = note.title.cleanupTitleForWebPath()
 
         val path = "${notesPath}/$dir/$instancePath/"
@@ -48,6 +47,10 @@ class NotesWebPathResolver(
 
     override fun resolveTagPage(tag: String): WebPagePath =
         indexWebPath("$notesPath/tags/$tag")
+
+    private companion object {
+        const val DraftsDirName = "drafts"
+    }
 }
 
 private fun indexWebPath(path: String) = WebPagePath(
