20bd66c933101f36d9f758245f1fac8fa35deea4
Denys M <dector9@gmail.com>
2020-06-26 13:26:10 +0400

    Include projects from `app/` dir

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,14 +7,22 @@ include(
     "templates-hyde"
 )
 
-// Include projects from `components/` dir
-File("components")
+fun findGradleProjectsIn(dir: String): Sequence<File> = File(dir)
     .listFiles()!!
     .asSequence()
     .filter { it.isDirectory }
     .filter { File(it, "build.gradle.kts").exists() }
-    .forEach { dir ->
-        val projectName = ":${dir.name}"
-        include(projectName)
-        project(projectName).projectDir = File("${dir.parent}/${dir.name}")
-    }
+
+// Include projects from `components/` dir
+findGradleProjectsIn("components").forEach { dir ->
+    val projectName = ":${dir.name}"
+    include(projectName)
+    project(projectName).projectDir = File("${dir.parent}/${dir.name}")
+}
+
+// Include projects from `apps/` dir with `app-` prefix
+findGradleProjectsIn("apps").forEach { dir ->
+    val projectName = ":app-${dir.name}"
+    include(projectName)
+    project(projectName).projectDir = File("${dir.parent}/${dir.name}")
+}
