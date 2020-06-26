bccf422b9256ea6c64d727149ddf401c89b56cdd
Denys M <dector9@gmail.com>
2020-06-26 22:49:56 +0400

    Move file-watch logic to `ServeApp`

--- apps/serve/src/main/kotlin/io/github/dector/glow/serve/ServeApp.kt
+++ apps/serve/src/main/kotlin/io/github/dector/glow/serve/ServeApp.kt
@@ -1,19 +1,57 @@
 package io.github.dector.glow.serve
 
 import io.github.dector.glow.config.LaunchConfig
+import io.github.dector.glow.config.RuntimeConfig
+import io.github.dector.glow.core.WebPage
+import io.github.dector.glow.core.components.GlowEngine
 import io.github.dector.glow.core.config.provideProjectConfig
 import io.github.dector.glow.di.DI
+import io.github.dector.glow.di.buildGlowEngine
+import io.github.dector.glow.di.get
 import io.github.dector.glow.di.provide
 import io.github.dector.glow.server.Server
+import io.github.dector.glow.server.components.InMemoryDataPublisher
+import io.github.dector.glow.utils.Execution
+import io.github.dector.glow.utils.StopWatch
+import io.github.dector.glow.utils.measureTimeMillis
+import io.github.dector.ktx.FileWatcher
 import java.io.File
+import java.nio.file.StandardWatchEventKinds.ENTRY_CREATE
+import java.nio.file.StandardWatchEventKinds.ENTRY_DELETE
+import java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY
 
 
 class ServeApp private constructor(
+    private val storage: MutableSet<WebPage>,
+    private val engine: GlowEngine,
     private val server: Server
 ) {
 
     fun execute() {
+        buildAndServeBlog()
+
+        watchForBlogSources {
+            buildAndServeBlog()
+        }
         server.run()
+    }
+
+    private fun buildAndServeBlog() {
+        println("Building blog...")
+        storage.clear()
+
+        val executionResult: Execution<GlowEngine.ExecutionResult> = measureTimeMillis {
+            engine.execute()
+        }
+
+        executionResult.error.let { error ->
+            if (error != null) {
+                System.err.println("Failed: ${error.message}")
+            }
+        }
+
+        val executionTime = StopWatch.DefaultSecondsFormatter(executionResult.time)
+        println("Ready! (finished in $executionTime)")
     }
 
     companion object {
@@ -27,7 +65,32 @@ class ServeApp private constructor(
 
             DI.provide(provideProjectConfig(projectDir, launchConfig))
 
-            return ServeApp(Server())
+            val storage = mutableSetOf<WebPage>()
+            val engine = provideGlowEngine(storage)
+
+            return ServeApp(
+                storage,
+                engine,
+                Server(storage)
+            )
         }
     }
+}
+
+private fun provideGlowEngine(pagesStorage: MutableSet<WebPage>) =
+    buildGlowEngine(
+        publisher = InMemoryDataPublisher(pagesStorage)
+    )
+
+private fun watchForBlogSources(body: () -> Unit) {
+    val sourcesFolder = DI.get<RuntimeConfig>()
+        .glow
+        .sourceDir
+        .toFile()
+    println("Serving '${sourcesFolder.absolutePath}'")
+
+    FileWatcher().watchRecursively(
+        sourcesFolder,
+        ENTRY_CREATE, ENTRY_MODIFY, ENTRY_DELETE
+    ) { body() }
 }
--- apps/serve/src/main/kotlin/io/github/dector/glow/server/Server.kt
+++ apps/serve/src/main/kotlin/io/github/dector/glow/server/Server.kt
@@ -1,26 +1,13 @@
 package io.github.dector.glow.server
 
-import io.github.dector.glow.config.RuntimeConfig
 import io.github.dector.glow.core.WebPage
-import io.github.dector.glow.core.components.GlowEngine
 import io.github.dector.glow.di.DI
-import io.github.dector.glow.di.buildGlowEngine
 import io.github.dector.glow.di.get
-import io.github.dector.glow.server.components.InMemoryDataPublisher
-import io.github.dector.glow.utils.Execution
-import io.github.dector.glow.utils.StopWatch.Companion.DefaultSecondsFormatter
-import io.github.dector.glow.utils.measureTimeMillis
-import io.github.dector.ktx.FileWatcher
 import io.javalin.Javalin
-import java.nio.file.StandardWatchEventKinds.ENTRY_CREATE
-import java.nio.file.StandardWatchEventKinds.ENTRY_DELETE
-import java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY
 
-class Server {
-
-    private val pagesStorage = mutableSetOf<WebPage>()
-
-    private val glowEngine = provideGlowEngine(pagesStorage)
+class Server(
+    pagesStorage: MutableSet<WebPage>
+) {
 
     private val app: Javalin = Javalin.create { config ->
         config.showJavalinBanner = false
@@ -30,43 +17,6 @@ class Server {
 
     fun run() {
         startServer(PORT)
-        buildAndServeBlog()
-
-        watchForBlogSources {
-            buildAndServeBlog()
-        }
-    }
-
-
-    private fun watchForBlogSources(body: () -> Unit) {
-        val sourcesFolder = DI.get<RuntimeConfig>()
-            .glow
-            .sourceDir
-            .toFile()
-        println("Serving '${sourcesFolder.absolutePath}'")
-
-        FileWatcher().watchRecursively(
-            sourcesFolder,
-            ENTRY_CREATE, ENTRY_MODIFY, ENTRY_DELETE
-        ) { body() }
-    }
-
-    private fun buildAndServeBlog() {
-        println("Building blog...")
-        pagesStorage.clear()
-
-        val executionResult: Execution<GlowEngine.ExecutionResult> = measureTimeMillis {
-            glowEngine.execute()
-        }
-
-        executionResult.error.let { error ->
-            if (error != null) {
-                System.err.println("Failed: ${error.message}")
-            }
-        }
-
-        val executionTime = DefaultSecondsFormatter(executionResult.time)
-        println("Ready! (finished in $executionTime)")
     }
 
     private fun startServer(port: Int) {
@@ -80,8 +30,3 @@ class Server {
 }
 
 private const val PORT = 9217
-
-private fun provideGlowEngine(pagesStorage: MutableSet<WebPage>) =
-    buildGlowEngine(
-        publisher = InMemoryDataPublisher(pagesStorage)
-    )
