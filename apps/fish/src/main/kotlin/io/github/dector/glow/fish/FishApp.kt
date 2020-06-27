40cb937fae2bfd0703bfe20583fb85371b4783b4
Denys M <dector9@gmail.com>
2020-06-27 13:29:04 +0400

    Add some docs to `fish` app

--- apps/fish/src/main/kotlin/io/github/dector/glow/fish/FishApp.kt
+++ apps/fish/src/main/kotlin/io/github/dector/glow/fish/FishApp.kt
@@ -3,6 +3,9 @@ package io.github.dector.glow.fish
 import io.github.dector.glow.ui.UiConsole
 
 
+/**
+ * Draw fish and exit.
+ */
 class FishApp private constructor(
     private val ui: UiConsole
 ) {
