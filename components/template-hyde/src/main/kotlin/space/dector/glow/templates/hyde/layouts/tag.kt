e28121983aee7274a0eafe628ea6907a94817d1d
Denys M <dector@dector.space>
2020-08-13 20:13:59 +0400

    Drop Hyde files

--- components/template-hyde/src/main/kotlin/space/dector/glow/templates/hyde/layouts/tag.kt
+++
@@ -1,8 +0,0 @@
-package space.dector.glow.templates.hyde.layouts
-
-import kotlinx.html.DIV
-import space.dector.glow.engine.RenderContext
-import space.dector.glow.plugins.notes.NoteVM
-
-fun DIV.tagPageContent(notes: List<NoteVM>, tag: String, context: RenderContext) =
-    notesIndexContent(notes, context = context)
